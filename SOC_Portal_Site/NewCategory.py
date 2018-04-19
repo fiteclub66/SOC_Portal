dd6a750d19a94362d058712c7eec491aea52720c
Brett Warner <bwarner159@hotmail.com>
2018-04-18 20:07:20 -0400

    Create NewCategory.py

---
+++ SOC_Portal_Site/NewCategory.py
@@ -0,0 +1,22 @@
+import mysql.connector
+from flask import Flask, render_template
+from tkinter.constants import INSERT
+
+def getDB():
+    return mysql.connector.connect(user='root', password='MyR00t1423!',
+                              host='10.0.51.21',
+                             database='SOC_Portal')
+    
+def insertCategoryData(cSID, ca, sS):
+    db = getDB()
+    
+    cur = db.cursor(buffered=True)
+    insertCategory = ("INSERT INTO CategoryRelations(catSkillID, category, skillset) Values(%s, %s, %s)")
+    
+    categoryData = (cSID, cat, sS)
+    
+    cur.execute(insertCategory, categoryData)
+    
+    db.commit()
+    cur.close()
+    db.close()
