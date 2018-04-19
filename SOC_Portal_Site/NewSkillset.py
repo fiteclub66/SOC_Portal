093283a3cbefd9b9674526475753c6f44f1d4e52
Brett Warner <bwarner159@hotmail.com>
2018-04-18 20:07:50 -0400

    Create NewSkillset.py

---
+++ SOC_Portal_Site/NewSkillset.py
@@ -0,0 +1,22 @@
+import mysql.connector
+from flask import Flask, render_template
+from tkinter.constants import INSERT
+
+def getDB():
+    return mysql.connector.connect(user='root', password='MyR00t1423!',
+                              host='10.0.51.21',
+                             database='SOC_Portal')
+    
+def insertSkillsetData(cSID, cat, sS):
+    db = getDB()
+    
+    cur = db.cursor(buffered=true)
+    insertSkillset = ("INSERT INTO CategoryRelations(catSkillID, category, skillset) Values(%s, %s, %s)")
+    
+    skillsetData = (cSID, cat, sS)
+    
+    cur.execute(insertSkillset, skillsetData)
+    
+    db.commit()
+    cur.close()
+    db.close()
